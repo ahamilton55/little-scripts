70d4e780800df36d5fbb8fa29a9851154770b223
Andrew Hamilton <ahamilton55@gmail.com>
2012-11-08 16:04:21 -0800

    Broke some of the main() into different methods
    - get_users pulls apart the owners of each group of system on the cobbler
      system
    - display_users shows the users and number of systems currently used by each
    - display_groups shows the groups and the number of remaining systems in each

--- cobbler-accounting/cobbler-accounting.py
+++ cobbler-accounting/cobbler-accounting.py
@@ -9,10 +9,57 @@ import operator
 from ConfigParser import SafeConfigParser
 from optparse import OptionParser
 
+def get_users(remote):
+    """Get the users of all systems current on the Cobbler server"""
+    users = {}
+    systems = remote.find_system({'name': '*'})
+
+    for system in systems:
+        system_info = remote.get_system(system)
+        owners = system_info['owners']
+        for owner in owners:
+            if owner in users:
+                users[owner] += 1
+            else:
+                users[owner] = 1
+
+    return sorted(users.iteritems(), key=operator.itemgetter(1),
+                            reverse=True)
+
+def display_users(users, groups):
+    """Print out the users and number of taken systems"""
+    user_title = "User\t\t# of systems taken"
+    print user_title
+    print "-"*len(user_title.expandtabs())
+
+    for user in users:
+        if not user[0] in groups:
+            if len(user[0]) >= 8:
+                print "{0}\t{1}".format(user[0], user[1])
+            else:
+                print "{0}\t\t{1}".format(user[0], user[1])
+
+    print
+
+def display_groups(users, groups):
+    """Print out the groups and the number of remaining systems"""
+    group_title = "Group\t\t# of Systems remaining"
+    print group_title
+    print "-"*len(group_title.expandtabs())
+
+    for user in users:
+        if user[0] in groups:
+            if len(user[0]) >= 8:
+                print "{0}\t{1}".format(user[0], user[1])
+            else:
+                print "{0}\t\t{1}".format(user[0], user[1])
+
+    print
+
 def main():
+    """Main method where the magic begins...really"""
     groups = ""
     cblr_url = ""
-    users = {}
 
     parser = OptionParser()
     parser.add_option("-c", "--config", action="store", dest="config",
@@ -22,7 +69,8 @@ def main():
     parser.add_option("-s", "--server", action="store", dest="server",
                 type="string", help="Cobbler server hostname or IP")
 
-    (options, args) = parser.parse_args()
+    #(options, args) = parser.parse_args()
+    options = parser.parse_args()[0]
 
     if options.server:
         cblr_url = "http://{0}/cobbler_api".format(options.server)
@@ -36,47 +84,10 @@ def main():
 
     remote = xmlrpclib.Server(cblr_url)
 
-    systems = remote.find_system({'name': '*'})
+    users = get_users(remote)
 
-    for system in systems:
-        system_info = remote.get_system(system)
-        owners = system_info['owners']
-        for owner in owners:
-            if owner in users:
-                users[owner] += 1
-            else:
-                users[owner] = 1
-
-    sorted_users = sorted(users.iteritems(), key=operator.itemgetter(1),
-                            reverse=True)
-
-    #Print out the users and taken systems
-    user_title = "User\t\t# of systems taken"
-    print user_title
-    print "-"*len(user_title.expandtabs())
-
-    for user in sorted_users:
-        if not user[0] in groups:
-            if len(user[0]) >= 8:
-                print "{0}\t{1}".format(user[0], user[1])
-            else:
-                print "{0}\t\t{1}".format(user[0], user[1])
-
-    print
-
-    # Print out the groups and remaining systems
-    group_title = "Group\t\t# of Systems remaining"
-    print group_title
-    print "-"*len(group_title.expandtabs())
-
-    for user in sorted_users:
-        if user[0] in groups:
-            if len(user[0]) >= 8:
-                print "{0}\t{1}".format(user[0], user[1])
-            else:
-                print "{0}\t\t{1}".format(user[0], user[1])
-
-    print
+    display_users(users, groups)
+    display_groups(users, groups)
 
 if __name__ == "__main__":
     main()
